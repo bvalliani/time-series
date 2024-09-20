3fc3a984e4a377f9bb74b484300da55ab860f4fb
Beenish Valliani <bvalliani@Beenishs-MBP.cable.rcn.com>
2024-09-20 17:20:12 -0400

    Training model

--- ts-model.py
+++ ts-model.py
@@ -6,70 +6,43 @@ from torch.nn import functional as F
 
 # ------------- HYPERPARAMTERS -------------
 
-N_CLASSES = 10000
-BATCH_SIZE = ...
+N_BINS = 10000
+BATCH_SIZE = 256
 WINDOW_SIZE = 512
-EMBED_SIZE = 100
+EMBED_SIZE = 512
 N_BLOCKS = 6
 N_HEADS = 8
 HEAD_SIZE = 512
 LEARNING_RATE = 1e-3
-EPOCHS = ...
+EPOCHS = 1000
 
 # ----------------- DATA -----------------
 # STEP 1: Read in CSV file.
 # STEP 2: Extract the column with the temperature.
-# STEP 3: Reshape data into training examples.
-# STEP 4: Scale the data.
-# STEP 5: Quantize the data.
+# STEP 3: Quantize the data.
 
-# STEP 1.
 train_file = read_csv("./data/DailyDelhiClimateTrain.csv")
 test_file = read_csv("./data/DailyDelhiClimateTest.csv")
 
-# STEP 2.
-train_data = train_file['meantemp'].tolist()
-test_data = test_file['meantemp'].tolist()
+train_data = np.array(train_file['meantemp'].tolist())
+test_data = np.array(test_file['meantemp'].tolist())
 
-# STEP 3.
+# Quantize training data.
+min = np.floor(np.min(train_data)) - 1e-3
+max = np.ceil(np.max(train_data))
+step_size = (max - min) / N_BINS
+bins = np.flip(np.arange(start=max, stop=min, step=-step_size))
+train_data = np.digitize(train_data, bins)
 
-# Training data.
-train_data = np.array(train_data)
-train_examples = []
-for i in range(len(train_data) - WINDOW_SIZE + 1):
-    train_examples.append(train_data[i:i+WINDOW_SIZE])
+# Quantize testing data.
+min = np.floor(np.min(test_data)) - 1e-3
+max = np.ceil(np.max(test_data))
+step_size = (max - min) / N_BINS
+bins = np.flip(np.arange(start=max, stop=min, step=-step_size))
+test_data = np.digitize(test_data, bins)
 
-train_examples = np.array(train_examples)
-
-# Testing data.
-test_data = np.array(test_data)
-test_examples = []
-for i in range(len(test_data) - WINDOW_SIZE + 1):
-    test_examples.append(test_data[i:i+WINDOW_SIZE])
-
-test_examples = np.array(test_examples)
-
-# STEP 4.
-train_examples /= np.sum(train_examples, axis=1, keepdims=True)
-test_examples /= np.sum(test_examples, axis=1, keepdims=True)
-
-# STEP 5.
-
-# Training data.
-train_min = np.floor(np.min(train_examples)) - 1e-6
-train_max = np.ceil(np.max(train_examples))
-step_size = (train_max - train_min) / N_CLASSES
-
-bins = np.flip(np.arange(start=train_max, stop=train_min, step=-step_size))
-quantized_train_examples = np.digitize(train_examples, bins)
-
-# Testing data.
-test_min = np.floor(np.min(test_examples)) - 1e-6
-test_max = np.ceil(np.max(test_examples))
-step_size = (test_max - test_min) / N_CLASSES
-
-bins = np.flip(np.arange(start=test_max, stop=test_min, step=-step_size))
-quantized_test_examples = np.digitize(test_examples, bins)
+train_data = torch.tensor(train_data, dtype=torch.long)
+test_data = torch.tensor(test_data, dtype=torch.long)
 
 # ------------------------ MODEL ------------------------
 
@@ -92,7 +65,7 @@ class AttentionHead(nn.Module):
         attention_matrix = head_size**-0.5 * (queries @ keys.transpose(-2, -1))     # (batch_size, window_size, window_size)
         if self.use_mask:
             attention_matrix = torch.tril(attention_matrix)
-            attention_matrix = torch.Tensor.masked_fill_(attention_matrix == 0, float('-inf'))
+            attention_matrix = attention_matrix.masked_fill(attention_matrix == 0, float('-inf'))
 
         attention_matrix = F.softmax(attention_matrix, dim=-1)
         output = attention_matrix @ values      # (batch_size, window_size, head_size)
@@ -103,7 +76,7 @@ class MultiHeadedAttention(nn.Module):
     def __init__(self, n_heads, input_size, output_size, use_mask):
         super().__init__()
         self.attention_heads = nn.ModuleList([AttentionHead(input_size, output_size // n_heads, use_mask) for _ in range(n_heads)])
-        self.projection = nn.Linear(n_heads*output_size, n_heads*output_size)
+        self.projection = nn.Linear(output_size, output_size)
 
     def forward(self, input):
         output = torch.cat([head(input) for head in self.attention_heads], dim=-1)
@@ -135,19 +108,19 @@ class TransformerBlock(nn.Module):
 
     def forward(self, input):
         output = input + self.attention(input)
-        output += self.layer_norm1(output)
-        output += self.feed_forward(output)
-        output += self.layer_norm2(output)
+        output = output + self.layer_norm1(output)
+        output = output + self.feed_forward(output)
+        output = output + self.layer_norm2(output)
         return output
 
 class Transformer(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.token_embedding = nn.Embedding(N_CLASSES, EMBED_SIZE)
+        self.token_embedding = nn.Embedding(N_BINS, EMBED_SIZE)
         self.position_encoding = nn.Embedding(WINDOW_SIZE, EMBED_SIZE)
         self.blocks = nn.Sequential(*[TransformerBlock(N_HEADS, EMBED_SIZE, HEAD_SIZE) for _ in range(N_BLOCKS)])
-        self.feed_forward = nn.Linear(EMBED_SIZE, N_CLASSES)
+        self.feed_forward = nn.Linear(EMBED_SIZE, N_BINS)
 
         self.apply(self.init_weights)
 
@@ -159,25 +132,37 @@ class Transformer(nn.Module):
 
     def forward(self, input, labels):
         token_embedding = self.token_embedding(input)
-        position_encoding = self.position_encoding(input)
-
+        position_encoding = self.position_encoding(torch.arange(input.shape[-1]))
         input = token_embedding + position_encoding
+
         input = self.blocks(input)
         logits = self.feed_forward(input)
 
-        batch_size, window_size, embed_size = logits.shape
-        logits = torch.reshape(logits, (batch_size*window_size, embed_size))
+        batch_size, _, embed_size = logits.shape
+        logits = logits[:, -1, :].reshape(batch_size, embed_size)
         labels = torch.flatten(labels)
         loss = F.cross_entropy(logits, labels)
 
         return logits, loss
 
+
+def batch():
+    indices = torch.randint(low=0, high=len(train_data) - WINDOW_SIZE, size=(BATCH_SIZE,))
+    examples = torch.stack([train_data[i:i+WINDOW_SIZE] for i in indices])
+    labels = torch.tensor([train_data[i+WINDOW_SIZE] for i in indices], dtype=torch.long)
+    return examples, labels
+
+
 # Build and train the model.
 GPTmodel = Transformer()
 optimizer = torch.optim.AdamW(GPTmodel.parameters(), lr=LEARNING_RATE)
 
-for _ in range(EPOCHS):
-    logits, loss = GPTmodel(train_data, ...)
+for e in range(EPOCHS):
+    examples, labels = batch()
+    logits, loss = GPTmodel(examples, labels)
     optimizer.zero_grad(set_to_none=True)
+    torch.autograd.set_detect_anomaly(True)
     loss.backward()
     optimizer.step()
+
+    print(f"Epoch: {e}, Loss: {loss}")
